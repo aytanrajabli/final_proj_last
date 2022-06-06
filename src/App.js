d8c7af912f628a62e8ab298cf6bb5d70e8879078
Ayten <aytenrecebli45@gmail.com>
2022-06-06 14:07:20 +0400

    khb

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import { Routes ,Route } from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/products" element={<Products/>} />
+        <Route exact path="/products/:id" element={<Product/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
